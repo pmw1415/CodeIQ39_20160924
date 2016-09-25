6b9fa3f21772d61d3141855ffcd646b26d4c6ee4
Daiki Murata <pmw1415.rkmbl@gmail.com>
2016-09-25 15:34:25 +0900

    Program.cs: リファクタリング

--- CodeIQ39_20160924/Program.cs
+++ CodeIQ39_20160924/Program.cs
@@ -1,36 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CodeIQ39_20160924
 {
     class Program
     {
-        static int getdigit(double src)
+        /*
+         * 桁数を算出
+         */
+        static int getDigit(double value)
         {
-            int val = 0;
-            while (!(src < 1.0))
+            int digit = 0;
+            while (!(value < 1.0))
             {
-                val++;
-                src /= 10.0;
+                digit++;
+                value /= 10.0;
             }
-            return val;
+            return digit;
         }
 
-        static double flip(double src)
+        /*
+         * 数値の各桁の値を逆順にした値に変換
+         */
+        static double flip(double value)
         {
-            double val = 0, tmp;
-            int digit = getdigit(Math.Truncate(src));
+            double flipValue = 0, tmp;
+            int digit = getDigit(Math.Truncate(value));
             for (int i = 0; i < digit; i++)
             {
-                tmp = (src / Math.Pow(10, i)) % 10;
-                val = Math.Truncate(val) * 10 + Math.Truncate(tmp);
+                tmp = (value / Math.Pow(10, i)) % 10;
+                flipValue = Math.Truncate(flipValue) * 10 + Math.Truncate(tmp);
             }
-            return Math.Truncate(val);
+            return Math.Truncate(flipValue);
         }
 
+        /*
+         * 数値が回文数かチェック
+         */
         static bool isAnagramNumber(double value)
         {
             Console.WriteLine("  値チェック: " + Math.Truncate(value) + " | " + Math.Truncate(flip(value)));
@@ -43,11 +48,11 @@ namespace CodeIQ39_20160924
              * 自然数Nに対して、Nを逆に並べた数を足し続けると回文数になることがある。
              * これを25回繰り返しても回文数にならない最小のNを見つける。
              */
-            double number = 1, value = 0;
+            double number = 0, value = 0;
             bool result;
 
             Console.WriteLine("start");
-            while (Math.Truncate(number) < 10000.0)
+            while (Math.Truncate(++number) < 10000.0)
             {
                 value = number;
                 Console.WriteLine("自然数: " + value);
@@ -62,6 +67,7 @@ namespace CodeIQ39_20160924
                         break;
                     }
 
+                    // 逆に並べた値を足す
                     value += Math.Truncate(flip(value));
                 }
 
@@ -70,7 +76,6 @@ namespace CodeIQ39_20160924
                     Console.WriteLine("回文数にならない数値ゲット: " + number);
                     break;
                 }
-                number += 1.0;
             }
             Console.WriteLine("end");
             Console.Read();
